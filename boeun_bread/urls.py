efbf0b6aafcf27ecba379627dbfe88c1fb4047ab
한재호 <gha2787@gmail.com>
2020-01-08 13:20:27 +0900

    login

--- boeun_bread/urls.py
+++ boeun_bread/urls.py
@@ -5,4 +5,7 @@ app_name = 'boeun_bread'
 
 urlpatterns = [
     path('',views.main,name="main"),
+    path('boeun/Login',views.Login, name="Login"),
+    path('boeun/SignUp',views.SignUp, name="SignUp"),
+    path('boeun/Login/LoginPage',views.LoginPage, name="LoginPage"),
 ]
