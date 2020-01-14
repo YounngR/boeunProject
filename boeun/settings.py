19f095b2b21ae767cbd85e8836a3055a550dc124
SYR <syr94@daum.net>
2020-01-14 16:03:34 +0900

    메인디자인2

--- boeun/settings.py
+++ boeun/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # 일반 설정
-LOGIN_URL = '/boeun/Login'
+LOGIN_URL = '/Login'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
